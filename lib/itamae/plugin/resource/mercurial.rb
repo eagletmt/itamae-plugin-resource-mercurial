4658b9bec79280bb6ada74b7dfde8c67ef6c12c0
Kohei Suzuki <eagletmt@gmail.com>
2014-10-13 13:41:49 +0900

    bundle gem itamae-plugin-resource-mercurial

---
+++ lib/itamae/plugin/resource/mercurial.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/mercurial/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Mercurial
+        # Your code goes here...
+      end
+    end
+  end
+end
